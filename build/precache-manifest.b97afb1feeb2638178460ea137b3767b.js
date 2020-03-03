e70c90c0ecbf1a430d8bcb6c8b11d3c66fbe2f8b
Rishabh Anand <f20170111@goa.bits-pilani.ac.in>
2020-03-03 22:39:47 +0530

    new production branch for release on netlify

---
+++ build/precache-manifest.b97afb1feeb2638178460ea137b3767b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7623f388f026fd097128385e3b0910a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dedffeea14f988091548",
+    "url": "/static/css/2.3208d2e3.chunk.css"
+  },
+  {
+    "revision": "68d9235615a02b3b0d6b",
+    "url": "/static/css/main.e6ef5944.chunk.css"
+  },
+  {
+    "revision": "dedffeea14f988091548",
+    "url": "/static/js/2.787aceb4.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.787aceb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68d9235615a02b3b0d6b",
+    "url": "/static/js/main.4321ad85.chunk.js"
+  },
+  {
+    "revision": "60ac2cbdcfb606687833",
+    "url": "/static/js/runtime-main.a5106b22.js"
+  },
+  {
+    "revision": "397d03b8382b22b167a5a7f0e85ab7cd",
+    "url": "/static/media/caret.397d03b8.svg"
+  },
+  {
+    "revision": "4694ce78850bdcceba29f8771ae4401e",
+    "url": "/static/media/directory.4694ce78.svg"
+  }
+]);
