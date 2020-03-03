44d39f5d8faac8c845424ca0c75a205afc4499f2
Rishabh Anand <f20170111@goa.bits-pilani.ac.in>
2020-03-03 22:17:45 +0530

    clearing up init state of reducer directorytree

--- src/Redux/DirectoryTree/index.js
+++ src/Redux/DirectoryTree/index.js
@@ -3,11 +3,6 @@ import { Tree } from './DataStructure/tree';
 import * as Actions from './Actions';
 const root = new Node('root', []);
 const directoryTree = new Tree(root);
-directoryTree.addNode('First', 'root');
-directoryTree.addNode('Second', 'root');
-directoryTree.addNode('Third', 'Second');
-directoryTree.addNode('Fourth', 'First');
-directoryTree.addNode('Fifth', 'First');
 const initState = {
     mainTree: directoryTree,
 };
